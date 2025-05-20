3880c55a8926a46c71380038b5a621ee4d1b9ebe
mike20 <gab94569456@gmail.com>
2025-05-20 13:17:39 -0600

    Main

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,5 +1,6 @@
+// Estudiante.java
 public class Estudiante {
-    // Atributos del estudiante
+    // Atributos
     private String nombre;
     private String matricula;
     private String carrera;
@@ -16,14 +17,5 @@ public class Estudiante {
         System.out.println("Nombre: " + nombre);
         System.out.println("Matrícula: " + matricula);
         System.out.println("Carrera: " + carrera);
-    }
-
-    // Método principal para ejecutar el programa
-    public static void main(String[] args) {
-        // Crear un objeto Estudiante
-        Estudiante estudiante = new Estudiante("Miguel", "M1234", "Ingeniería en Sistemas");
-
-        // Imprimir los datos del estudiante
-        estudiante.imprimirDatos();
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        // Crear un objeto Estudiante
+        Estudiante estudiante1 = new Estudiante("Miguel", "A1234", "Ingeniería en Sistemas");
+
+        // Imprimir los datos del estudiante
+        estudiante1.imprimirDatos();
+    }
+}
